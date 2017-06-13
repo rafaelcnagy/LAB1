fc7893fa93c5652b8a4a49b2f6474764790726d3
rafael correa nagy <rafaelnagy@s147pc115.dcc.ufba.br>
2017-06-13 19:01:50 -0300

    Continuando a lista 3...

---
+++ Lista3/esfinge-WA.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main(){
+	
+	int teste;
+	cin>>teste;
+
+	for(int cont=0; cont<teste; cont++){
+
+		string numero;
+		cin>>numero;
+		queue <char> num;
+		for(int i=0; i<numero.size(); i++)
+			num.push(numero[i]);
+		//num.push('x');
+
+		int bira;
+		cin>>bira;
+
+		cout<<numero<<endl;
+
+		for(int i=0; i<bira; i++){
+
+			char anterior=num.front();
+			int qnt=0;
+			queue <char> ward;
+
+			while(!num.empty()){
+
+				while(anterior==num.front()){
+					qnt++;
+					num.pop();
+				}
+				cout<<qnt<<anterior;
+				ward.push(qnt+'0');
+				ward.push(anterior);
+				if(!num.empty()){
+					anterior=num.front();
+					qnt=0;
+				}
+			}
+
+			//cout<<endl<<endl<<"ward=";
+
+			while(!ward.empty()){
+				//cout<<ward.front();
+				num.push(ward.front());
+				ward.pop();
+			}
+			cout<<endl;
+		}
+		if(cont!=teste-1)
+			cout<<endl;
+	}
+}
